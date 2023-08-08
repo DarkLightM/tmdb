d7c760527ead0195bc2439c44ccafa60ea90ea0d
maxgubar <max.gubar@mail.ru>
2023-08-08 11:29:58 +0500

    Done: ViewModel

--- app/src/main/java/com/example/tmdbkotlinapp/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/tmdbkotlinapp/ui/movie_details/MovieDetailsFragment.kt
@@ -1,32 +1,58 @@
 package com.example.tmdbkotlinapp.ui.movie_details
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.tmdbkotlinapp.MainApplication
 import com.example.tmdbkotlinapp.R
+import com.example.tmdbkotlinapp.databinding.FragmentMovieDetailsBinding
+import com.example.tmdbkotlinapp.databinding.FragmentPopularMoviesBinding
+import com.example.tmdbkotlinapp.di.ViewModelFactory
+import com.example.tmdbkotlinapp.ui.popular_movies.PopularMoviesViewModel
+import javax.inject.Inject
 
 class MovieDetailsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MovieDetailsFragment()
-    }
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
 
-    private lateinit var viewModel: MovieDetailsViewModel
+    private val movieDetailsViewModel by viewModels<MovieDetailsViewModel> { viewModelFactory }
+
+    private var _binding: FragmentMovieDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onAttach(context: Context) {
+        MainApplication.appComponent.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MovieDetailsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        movieDetailsViewModel.getMovieDetails(11)
+        movieDetailsViewModel.getMovieCast(11)
+
+        movieDetailsViewModel.movie.observe(viewLifecycleOwner){
+            Log.i("Details fragment", it.toString())
+        }
+
+        movieDetailsViewModel.cast.observe(viewLifecycleOwner){
+            Log.i("Details fragment", it.toString())
+        }
     }
 
 }
--- app/src/main/java/com/example/tmdbkotlinapp/ui/movie_details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/tmdbkotlinapp/ui/movie_details/MovieDetailsViewModel.kt
@@ -1,7 +1,37 @@
 package com.example.tmdbkotlinapp.ui.movie_details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tmdbkotlinapp.domain.models.Actor
+import com.example.tmdbkotlinapp.domain.models.Movie
+import com.example.tmdbkotlinapp.domain.repository.CastRepository
+import com.example.tmdbkotlinapp.domain.repository.MovieRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MovieDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MovieDetailsViewModel @Inject constructor(
+    private val castRepository: CastRepository,
+    private val movieRepository: MovieRepository
+) : ViewModel() {
+    private val _movie = MutableLiveData<Movie>()
+    val movie: LiveData<Movie> get() = _movie
+
+    private val _cast = MutableLiveData<List<Actor>>()
+    val cast: LiveData<List<Actor>> get() = _cast
+
+
+    fun getMovieDetails(id: Int){
+        viewModelScope.launch {
+            _movie.postValue(movieRepository.getMovieDetails(id))
+        }
+    }
+
+    fun getMovieCast(id: Int){
+        viewModelScope.launch {
+            _cast.postValue(castRepository.getMovieCast(id))
+        }
+    }
+
 }
