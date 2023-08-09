a6563dee62c604db8def52b96b5bdfa45581c9a9
maxgubar <max.gubar@mail.ru>
2023-08-09 15:51:01 +0500

    Fixed: NumberUtils

--- app/src/main/java/com/example/tmdbkotlinapp/ui/utils/NumberUtils.kt
+++ app/src/main/java/com/example/tmdbkotlinapp/ui/utils/NumberUtils.kt
@@ -2,11 +2,8 @@ package com.example.tmdbkotlinapp.ui.utils
 
 class NumberUtils {
     companion object {
-        fun formatFloat(number: Float, numberOfDigits: Int? = null): Float {
-            return if (numberOfDigits == null) {
-                String.format("%.1f", number).replace(",", ".").toFloat()
-            } else
-                String.format("%.$numberOfDigits", number).replace(",", ".").toFloat()
+        fun formatFloat(number: Float, numberOfDigits: Int = 1): Float {
+            return String.format("%.${numberOfDigits}f", number).replace(",", ".").toFloat()
         }
     }
 }
