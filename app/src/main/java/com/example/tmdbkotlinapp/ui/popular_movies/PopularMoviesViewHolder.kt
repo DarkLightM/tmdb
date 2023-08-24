b3f9f9924a03a27949be73ece1aad5324a133367
maxgubar <max.gubar@mail.ru>
2023-08-24 17:12:59 +0500

    Improve popular screen

--- app/src/main/java/com/example/tmdbkotlinapp/ui/popular_movies/PopularMoviesViewHolder.kt
+++ app/src/main/java/com/example/tmdbkotlinapp/ui/popular_movies/PopularMoviesViewHolder.kt
@@ -1,5 +1,7 @@
 package com.example.tmdbkotlinapp.ui.popular_movies
 
+import android.text.Html
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.tmdbkotlinapp.R
@@ -12,14 +14,17 @@ class PopularMoviesViewHolder(private val binding: PopularMovieCardBinding) :
     private val context = binding.root.context
 
     fun bind(movie: Movie) {
-        binding.moviePoster.load(movie.posterPath)
-        binding.movieName.text =
-            String.format(context.getString(R.string.movie_name_format), movie.originalTitle)
-        binding.movieReleaseDate.text = String.format(
-            context.getString(R.string.movie_release_date_format), movie.releaseDate
-        )
-        binding.movieRating.text = String.format(
-            context.getString(R.string.movie_rating_format), movie.rating
-        )
+        binding.moviePoster.load(movie.posterPath) {
+            placeholder(R.drawable.ic_placeholder)
+            listener(onSuccess = { _, _ ->
+                binding.moviePoster.scaleType = ImageView.ScaleType.CENTER_CROP
+            })
+        }
+        binding.movieName.text = movie.originalTitle
+        binding.movieReleaseDate.text = Html.fromHtml(
+            String.format(
+                context.getString(R.string.movie_release_date_format), movie.releaseDate
+            ), Html.FROM_HTML_MODE_LEGACY)
+        binding.movieRating.text = movie.rating.toString()
     }
 }
