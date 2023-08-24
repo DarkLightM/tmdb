a5a1ec68866e5ec57dbf228ab808bba4032b3b68
maxgubar <max.gubar@mail.ru>
2023-08-24 17:21:30 +0500

    Improve saved screen

--- app/src/main/java/com/example/tmdbkotlinapp/ui/saved_movies/SavedMovieViewHolder.kt
+++ app/src/main/java/com/example/tmdbkotlinapp/ui/saved_movies/SavedMovieViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.tmdbkotlinapp.ui.saved_movies
 
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.tmdbkotlinapp.R
 import com.example.tmdbkotlinapp.databinding.SavedMoviesCardBinding
 import com.example.tmdbkotlinapp.domain.models.Movie
 
@@ -9,7 +10,9 @@ class SavedMovieViewHolder(private val binding: SavedMoviesCardBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
         fun bind(movie: Movie){
-            binding.moviePoster.load(movie.posterPath)
+            binding.moviePoster.load(movie.posterPath) {
+                placeholder(R.drawable.ic_placeholder)
+            }
             binding.movieName.text = movie.originalTitle
             binding.movieRating.text = movie.rating.toString()
             binding.movieMainGenre.text = movie.genreList?.firstOrNull()?.name ?: ""
